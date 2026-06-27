e04e7832824b0dbf46435e478b229591d5a80190
dakfildbanze0-cell <dakfildbanze0@gmail.com>
2026-06-27 16:50:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from scraper import pesquisar
+
+app = FastAPI(title="Boladas Scraper")
+
+class Pesquisa(BaseModel):
+    query: str
+
+@app.get("/")
+def home():
+    return {"status": "online"}
+
+@app.post("/search")
+def search(data: Pesquisa):
+    resultado = pesquisar(data.query)
+    return resultado
